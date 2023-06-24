14387959dd3fc762d5e44d88d527955c7d7abf54
JrSchmidtt <junior.schmidt.99@gmail.com>
2023-06-23 21:42:42 -0300

    add handler function

--- src/service/category.go
+++ src/service/category.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"context"
+
 	"github.com/JrSchmidtt/golang-grpc/src/database"
 	"github.com/JrSchmidtt/golang-grpc/src/pb"
 )
@@ -14,4 +16,20 @@ type CategoryService struct {
 // NewCategoryService is a function that returns a new CategoryService
 func NewCategoryService(categoryDB database.Category) *CategoryService {
 	return &CategoryService{CategoryDB: categoryDB}
+}
+
+// CreateCategory is a function handler that creates a new category
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{Category: categoryResponse}, nil
 }
